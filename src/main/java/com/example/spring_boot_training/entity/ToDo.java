96000e642e5be13b0ec07ba27491330d53780359
mbart <mbarth509@gmail.com>
2022-08-31 16:18:59 +0200

    connect_to_Docker

---
+++ src/main/java/com/example/spring_boot_training/entity/ToDo.java
@@ -0,0 +1,33 @@
+package com.example.spring_boot_training.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+@Entity
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String aufgabe;
+    private String datum;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn
+    private List<ToDo> meineToDoListe;
+
+
+
+}
