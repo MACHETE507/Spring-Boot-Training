562e8c3d43e2614525ee228bbfd513e402d855a3
mbart <mbarth509@gmail.com>
2022-09-08 11:05:06 +0200

    Todo_security_basic

---
+++ src/main/java/com/example/spring_boot_training/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.spring_boot_training.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername("marco")
+                .password("{noop}compusafe")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+
+//    public void config(HttpSecurity http) throws Exception {
+//        http
+//
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/todo").permitAll()
+//                .anyRequest().authenticated().and().httpBasic();
+//    }
+}
+
